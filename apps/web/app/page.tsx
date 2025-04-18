9cad02b20b25a75ea96170a9fa8319dcbf532794
Smnthjm08 <sumanthjmm.04@gmail.com>
2025-04-18 14:56:27 +0530

    addeed dev ci pipeline

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -17,7 +17,9 @@ export default async function Page() {
     return (
       <div className="flex items-center justify-center min-h-svh">
         <div className="flex flex-col items-center justify-center gap-4">
+          "dev ci-cd pipeline"
           <div className="text-blue-300">
+            
             id: {users[0]?.id}
             username: {users[0]?.username}
             password: {users[0]?.password}
